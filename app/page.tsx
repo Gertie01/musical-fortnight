b4e02332d4c2cc47d503a93d91603adcb06ec930
Gertie01 <67640799+Gertie01@users.noreply.github.com>
2026-03-10 09:36:12 -0400

    Add Home component with ImageStudio

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import ImageStudio from '@/components/ImageStudio';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <ImageStudio />
+    </main>
+  );
+}
