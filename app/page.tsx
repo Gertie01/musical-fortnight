b44132e7d11eda83d8089214738be7af8b06398b
Gertie01 <67640799+Gertie01@users.noreply.github.com>
2026-03-10 12:10:08 -0400

    Rename Home component to Page and simplify return

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import ImageStudio from '@/components/ImageStudio'
 
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black">
-      <ImageStudio />
-    </main>
-  );
+export default function Page() {
+  return <ImageStudio />
 }
